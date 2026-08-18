e66bace32c42ec2127b34cd7601e678f3ca0a264
totoday <totodayqiang@gmail.com>
2026-08-18 11:25:55 +0800

    Raise default memory consolidation threshold to 20 KiB (#689)

--- shared/server-settings.ts
+++ shared/server-settings.ts
@@ -60,7 +60,7 @@ export {
   ProviderRuntimeCommandsConfig,
 };
 
-export const DEFAULT_MEMORY_COHERENCE_CONSOLIDATION_THRESHOLD_BYTES = 16 * 1024;
+export const DEFAULT_MEMORY_COHERENCE_CONSOLIDATION_THRESHOLD_BYTES = 20 * 1024;
 export const DEFAULT_MAX_CONCURRENT_AGENT_RUNS = 5;
 
 export const RuntimeConfig = z.object({
